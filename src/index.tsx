af943ce9af7da10a6aedef3cec8364ba74e3f1db
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-04-02 13:56:45 -0300

    feat: add fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,27 @@
+import {
+  OpenSans_400Regular,
+  OpenSans_700Bold,
+  OpenSans_600SemiBold,
+} from '@expo-google-fonts/open-sans';
+import { Rosarivo_400Regular } from '@expo-google-fonts/rosarivo';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export function App() {
+  const [isFontsLoaded] = useFonts({
+    Rosarivo_400Regular,
+
+    OpenSans_400Regular,
+    OpenSans_600SemiBold,
+    OpenSans_700Bold,
+  });
+
+  if (!isFontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
